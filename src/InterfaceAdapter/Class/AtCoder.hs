2fdfe661186611bb3101b53d29238ee9a2565915
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2020-08-09 14:07:31 +0900

    Add AtCoder Class

---
+++ src/InterfaceAdapter/Class/AtCoder.hs
@@ -0,0 +1,36 @@
+module InterfaceAdapter.Class.AtCoder
+  ( AnonEff,
+    EffName,
+    HasEff,
+    NamedEff,
+    lift,
+    AtCoder (..),
+    createSession,
+  )
+where
+
+import Data.Extensible
+import Data.Extensible.Effect
+import Data.Proxy (Proxy (Proxy))
+import DomainObject.UserName (UserName)
+import DomainObject.UserPassword (UserPassword)
+import Essential hiding (lift)
+import Network.HTTP.Client (CookieJar)
+import InterfaceAdapter.Class.SessionRepository (Session)
+
+data AtCoder a where
+  CreateSession :: UserName -> UserPassword -> AtCoder Session
+
+type EffName = "AtCoder"
+
+type AnonEff = AtCoder
+
+type NamedEff = EffName >: AnonEff
+
+type HasEff effs = Lookup effs EffName AtCoder
+
+lift :: forall effs a. HasEff effs => AtCoder a -> Eff effs a
+lift = liftEff (Proxy @EffName)
+
+createSession :: HasEff effs => UserName -> UserPassword -> Eff effs Session
+createSession userName userPassword = lift (CreateSession userName userPassword)
--- src/Usecase/Login.hs
+++ src/Usecase/Login.hs
@@ -1,14 +1,16 @@
 module Usecase.Login (login) where
 
-import DomainObject.UserName (UserName (..))
-import DomainObject.UserPassword (UserPassword (..))
+import DomainObject.UserName (UserName)
+import DomainObject.UserPassword (UserPassword)
+import qualified InterfaceAdapter.Class.AtCoder as AtCoder
+import qualified InterfaceAdapter.Class.SessionRepository as SessionRepository
 import Usecase.Imports
 
 login ::
-  ( MonadIO m,
-    MonadThrow m
-  ) =>
+  (AtCoder.HasEff effs, SessionRepository.HasEff effs) =>
   UserName ->
   UserPassword ->
-  m ()
-login userName userPassword = error "TODO impl"
+  Eff effs ()
+login userName userPassword = do
+  session <- AtCoder.createSession userName userPassword
+  SessionRepository.store session
