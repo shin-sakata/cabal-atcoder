88911c6e53bd7f4a5b87955ca957d9f162e54a4a
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2020-08-19 16:25:28 +0900

    add

--- src/Usecase/TestTaskAnswer.hs
+++ src/Usecase/TestTaskAnswer.hs
@@ -2,6 +2,7 @@ module Usecase.TestTaskAnswer where
 
 import Domain.Object.TaskAnswer (TaskAnswer)
 import Domain.Object.Status (Status)
+import Essential
 
 testTaskAnswer :: MonadIO m => TaskAnswer -> m Status
 testTaskAnswer taskAnswer = undefined
